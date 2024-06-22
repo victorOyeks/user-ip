aef17d61eaff57c6e245c04ce349cdf5bbb34290
victorOyeks <bolaoyeks@gmail.com>
2024-06-22 22:27:35 +0100

    updated

--- Services/GreetingsService.cs
+++ Services/GreetingsService.cs
@@ -26,14 +26,20 @@ namespace hngstageone.Services
         {
             var context = _httpContextAccessor.HttpContext;
             string ip = context.Request.Headers["X-Forwarded-For"].FirstOrDefault();
-            if (string.IsNullOrEmpty(ip))
+            if (!string.IsNullOrEmpty(ip))
+            {
+                ip = ip.Split(',').First().Trim();
+                
+            }
+            else
             {
                 ip = context.Connection.RemoteIpAddress.ToString();
+                if (ip == "::1")
+                {
+                    ip = "127.0.0.1";
+                }
             }
-            if (ip == "::1")
-            {
-                ip = "127.0.0.1";
-            }
+            
             return Task.FromResult(ip);
         }
     }
